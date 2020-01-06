6433f06b767bcff196c09bc0697235e1b721db05
Kris Kasprzak <kris.kasprzak@yahoo.com>
2020-01-05 20:50:48 -0600

    Update ILI9341_t3.ino

--- Examples/Teensy/ILI9341_t3/ILI9341_t3.ino
+++ Examples/Teensy/ILI9341_t3/ILI9341_t3.ino
@@ -49,16 +49,14 @@ void setup() {
 }
 
 void loop() {
-
+  
+  // get some data
   i++;
   j += 0.0013;
-  // get some data
-
   thetime = millis();
   h = (thetime / 3600000) / 1000;
   m = (thetime / 1000 - (3600 * h)) / 60;
   s = (thetime / 1000 - (3600 * h) - (m * 60));
-
   sprintf(str, "Time: %02d:%02d:%02d", h, m, s);
 
 
@@ -70,7 +68,7 @@ void loop() {
   Display.fillRect(Display.getCursorX(), 0, 100, 35, C_RED);
   Display.print(i);
 
-
+  // now draw data usign flicker free print
   Display.setFont(Arial_24);
   Display.setCursor(10, 50);
   Display.setTextColor(C_WHITE, C_RED);
